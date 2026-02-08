6a63c61b6589bf759eeb61a9cd03fb7bb8081aab
Haochen Zhang <hchzhang@amazon.com>
2026-02-08 11:54:05 -0800

    add ios for app keeping

---
+++ OrangeLineTracker/OrangeLineIOS/OrangeLineIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeLineIOSApp.swift
+//  OrangeLineIOS
+//
+//  Created by Zhang, Haochen on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct OrangeLineIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrangeLineTracker/OrangeLineIOSTests/OrangeLineIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeLineIOSTests.swift
+//  OrangeLineIOSTests
+//
+//  Created by Zhang, Haochen on 2/8/26.
+//
+
+import Testing
+@testable import OrangeLineIOS
+
+struct OrangeLineIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
